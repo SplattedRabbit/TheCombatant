5fad4500362e670b3bc829bb6f99e4e23fa41e7c
SplattedRabbit <julianstyles@live.de>
2026-06-17 16:40:59 +0200

    build: update service-worker.js cache version

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dnd-combatsheet-v4.0.0-cache-v53';
+const CACHE_NAME = 'dnd-combatsheet-v4.0.0-cache-v54';
 const ASSETS = [
   './dist/assets/BaseDialogs-DxH0phdZ.js',
   './dist/assets/icon-192-BiJXAwPn.png',
