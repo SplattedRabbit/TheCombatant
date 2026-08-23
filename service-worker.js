94915c6a1400a26c86d2eb4132c62ee640091223
SplattedRabbit <julianstyles@live.de>
2026-08-23 19:34:34 +0200

    chore(sw): update service worker cache version hash

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dnd-combatsheet-v4.5.0-cache-v231';
+const CACHE_NAME = 'dnd-combatsheet-v4.5.0-cache-v232';
 const ASSETS = [
   './dist/assets/BaseDialogs-DxH0phdZ.js',
   './dist/assets/data-registry-BxWK3y79.js',
